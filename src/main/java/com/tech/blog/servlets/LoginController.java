5f6a13ce4244c837cae28a9f89b7189deb68c66f
nawaz quazi <nawazquazi356@gmail.com>
2023-03-25 16:50:52 +0530

    Project TechBlog

---
+++ src/main/java/com/tech/blog/servlets/LoginController.java
@@ -0,0 +1,105 @@
+package com.tech.blog.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import com.tech.blog.dao.UserDao;
+import com.tech.blog.entities.Message;
+import com.tech.blog.entities.User;
+import com.tech.blog.helper.ConnectionProvider;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Servlet LoginServlet</title>");
+			out.println("</head>");
+			out.println("<body>");
+//          login 
+//          fetch username and password from request
+			String userEmail = request.getParameter("email");
+			String userPassword = request.getParameter("password");
+
+			UserDao dao = null;
+			try {
+				dao = new UserDao(ConnectionProvider.getConnection());
+				User u = dao.getUserByEmailAndPassword(userEmail, userPassword);
+				if (u == null) {
+					// login.................
+//                    error///
+//                    out.println("Invalid Details..try again");
+					Message msg = new Message("Invalid Details ! try with another", "error", "alert-danger");
+					HttpSession s = request.getSession();
+					s.setAttribute("msg", msg);
+					response.sendRedirect("login.jsp");
+				} else {
+					// ......
+//                    login success
+					HttpSession s = request.getSession();
+					s.setAttribute("currentUser", u);
+					response.sendRedirect("profile.jsp");
+				}
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+			}
+
+			out.println("</body>");
+			out.println("</html>");
+		}
+	}
+
+	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
+	// + sign on the left to edit the code.">
+	/**
+	 * Handles the HTTP <code>GET</code> method.
+	 *
+	 * @param request  servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException      if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 *
+	 * @param request  servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException      if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}
+
+}
---
+++ src/main/java/com/tech/blog/servlets/RegisterController.java
@@ -0,0 +1,72 @@
+package com.tech.blog.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
+import com.tech.blog.dao.UserDao;
+import com.tech.blog.entities.User;
+import com.tech.blog.helper.ConnectionProvider;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class newa
+ */
+@WebServlet("/newa")
+public class RegisterController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+//            fetch all form data
+			String name = request.getParameter("user_name");
+			String email = request.getParameter("user_email");
+			String password = request.getParameter("user_password");
+			String gender = request.getParameter("gender");
+			String about = request.getParameter("about");
+			// create user object and set all data to that object..
+			User user = new User(name, email, password, gender, about);
+
+			// create a user dao object..
+			UserDao dao = new UserDao(ConnectionProvider.getConnection());
+			if (dao.saveUser(user)) {
+				request.setAttribute("status", "success");
+			} else {
+				request.setAttribute("status", "failed");
+			}
+			request.getRequestDispatcher("registration.jsp").forward(request, response);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
+
+}
